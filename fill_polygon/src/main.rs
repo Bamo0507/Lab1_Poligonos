35601e01f59cbcf60f22de00a53bea668bf9b463
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-07-12 00:47:22 -0600

    Poligono-2 (#2)

--- fill_polygon/src/main.rs
+++ fill_polygon/src/main.rs
@@ -8,21 +8,15 @@ use utils::draw::{polygon, fill_polygon};
 fn main() {
     let mut fb = Framebuffer::new(800, 600, Color::DARKBLUE);
 
-    // Estrella
-    let poly1 = vec![
-        Vector2::new(165.0, 380.0),
-        Vector2::new(185.0, 360.0),
-        Vector2::new(180.0, 330.0),
-        Vector2::new(207.0, 345.0),
-        Vector2::new(233.0, 330.0),
-        Vector2::new(230.0, 360.0),
-        Vector2::new(250.0, 380.0),
-        Vector2::new(220.0, 385.0),
-        Vector2::new(205.0, 410.0),
-        Vector2::new(193.0, 383.0),
+    // Cuadrado
+    let poly2 = vec![
+        Vector2::new(321.0, 335.0),
+        Vector2::new(288.0, 286.0),
+        Vector2::new(339.0, 251.0),
+        Vector2::new(374.0, 302.0),
     ];
-    polygon(&mut fb, &poly1, Color::WHITE);
-    fill_polygon(&mut fb, &poly1, Color::YELLOW, Color::WHITE);
+    polygon(&mut fb, &poly2, Color::WHITE);
+    fill_polygon(&mut fb, &poly2, Color::BLUE, Color::WHITE);
 
     // Exporatr imagen PNG
     fb.export_to_file("out.png");
