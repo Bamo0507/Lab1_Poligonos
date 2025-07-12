40f52e09267c510c309f1314e8a2450c8098def3
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-07-12 00:49:41 -0600

    Poligono-3

--- fill_polygon/src/main.rs
+++ fill_polygon/src/main.rs
@@ -8,15 +8,14 @@ use utils::draw::{polygon, fill_polygon};
 fn main() {
     let mut fb = Framebuffer::new(800, 600, Color::DARKBLUE);
 
-    // Cuadrado
-    let poly2 = vec![
-        Vector2::new(321.0, 335.0),
-        Vector2::new(288.0, 286.0),
-        Vector2::new(339.0, 251.0),
-        Vector2::new(374.0, 302.0),
+    //Triangulo
+    let poly3 = vec![
+        Vector2::new(377.0, 249.0),
+        Vector2::new(411.0, 197.0),
+        Vector2::new(436.0, 249.0),
     ];
-    polygon(&mut fb, &poly2, Color::WHITE);
-    fill_polygon(&mut fb, &poly2, Color::BLUE, Color::WHITE);
+    polygon(&mut fb, &poly3, Color::WHITE);
+    fill_polygon(&mut fb, &poly3, Color::RED, Color::WHITE);
 
     // Exporatr imagen PNG
     fb.export_to_file("out.png");
